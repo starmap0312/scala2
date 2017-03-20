5335aa8c02f8b62a7b611f89522cd24dd07a7516
kuanyu <starmap0312@gmail.com>
2017-03-20 20:50:29 +0800

    add more

--- src/main/scala/PartialApplication.scala
+++ src/main/scala/PartialApplication.scala
@@ -3,21 +3,24 @@
   */
 object PartialApplication {
   def main(args: Array[String]): Unit = {
-    def adder(m: Int, n: Int) = m + n   // (Int, Int) => Int
+    def adder(m: Int, n: Int) = m + n   // (m: Int, n: Int)Int: expression not yet evaluated
     println(adder(2, 3))                // 5
 
-    // we can obtain a partially applied function object (lambda) as below
-    val addTwo = adder(2, _: Int)       // Int => Int
+    val add = adder(_, _)               // (Int, Int) => Int: Function2
+    println(add(2, 3))                  // 5
+
+    // we can obtain a partially applied Function1 object as below
+    val addTwo = adder(2, _: Int)       // (Int) => Int: Function1
     println(addTwo(3))                  // 5
 
     // or curry the function as below
-    val curriedAdd = (adder _).curried  // Int => (Int => Int)
-    val addTwo2 = curriedAdd(2)
+    val curriedAdd = (adder _).curried  // Int => (Int => Int): Function1
+    val addTwo2 = curriedAdd(2)         // Int => Int: Function1
     println(addTwo2(3))                 // 5
 
     // or we can define as below
-    def adder2(m: Int)(n: Int) = m + n  // (Int, Int) => Int
-    val addTwo3 = adder2(2) _
+    def adder2(m: Int)(n: Int) = m + n  // (m: Int)(n: Int)Int: expression not yet evaluated
+    val addTwo3 = adder2(2) _           // Int => Int: Function1
     println(adder2(2)(3))               // 5
     println(addTwo3(3))                 // 5
   }
