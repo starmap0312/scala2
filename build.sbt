76c4a27fa53f2681b09ad357fb4265205c0e5b49
kuanyu <starmap0312@gmail.com>
2017-12-20 10:20:44 +0800

    add more

--- build.sbt
+++ build.sbt
@@ -6,6 +6,6 @@ scalaVersion := "2.12.1"
 
 libraryDependencies ++= Seq(
   "com.jsuereth" %% "scala-arm" % "2.0", "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.8.6",
-  "com.typesafe" % "config" % "1.2.1",
+  "com.typesafe" % "config" % "1.3.1",
   "junit" % "junit" % "4.11" % "test"
 )
--- src/test/scala/TypesafeConfigTest.scala
+++ src/test/scala/TypesafeConfigTest.scala
@@ -1,5 +1,16 @@
-import com.typesafe.config.{Config, ConfigFactory, ConfigObject}
+import com.typesafe.config.{Config, ConfigFactory, ConfigObject, ConfigBeanFactory}
 import scala.collection.JavaConverters._
+
+class MyConfigList {
+  var typ: String = _
+  var list: java.util.List[String] = _
+
+  def getType = this.typ
+  def setType(t: String) = this.typ = t
+
+  def getList = this.list
+  def setList(l: java.util.List[String]) = this.list = l
+}
 
 object TypesafeConfigTest {
 
@@ -16,16 +27,24 @@ object TypesafeConfigTest {
     //    get the nested Config instance by the requested path
     val conf1: Config = config.getConfig("configuration1")
     println(conf1)
-    // Config(SimpleConfigObject(
-    //   {
-    //     "field1":[{"text":"text1","type":"type1"},{"text":"text2","type":"type2"}],
-    //     "field2":[{"text":"text3","type":"type3"}]
-    //   }
+    // Config(
+    //   SimpleConfigObject(
+    //     {
+    //       "field1":[
+    //         {"text":"text1","type":"type1"},
+    //         {"list":["list1-1","list1-2"],"type":"type2"}
+    //       ],
+    //       "field2":[
+    //         {"text":"text2","type":"type3"}
+    //       ]
+    //     }
+    //   )
     // )
+
 
     // 3) Config.getConfigList([path: String]): returns a java.util.List[Config]
     //    Config.getString([path: String]): returns a String
-    println(config.getConfigList("configuration2.field2").asScala.toList.head.getString("text")) // text4
+    println(config.getConfigList("configuration1.field2").asScala.toList.head.getString("text")) // text2
 
     // 4) Config.root():
     //    get the ConfigObject
@@ -34,13 +53,17 @@ object TypesafeConfigTest {
     configObject.keySet().asScala.map(
       (field: String) => {
         val configList: List[Config] = conf1.getConfigList(field).asScala.toList
-        println(configList)        // List(Config(SimpleConfigObject({"text":"text3","type":"type3"})))
+        println(configList)
       }
-    )
+    ) // List(Config(SimpleConfigObject({"text":"text1","type":"type1"})), Config(SimpleConfigObject({"list":["list1-1","list1-2"],"type":"type2"})))
+      // List(Config(SimpleConfigObject({"text":"text2","type":"type3"})))
 
     // 5) ConfigBeanFactory.create[T]([Config], [Class[T]]):
-    //    ConfigBeanFactory
-    //    this creates an instance of Class[T] with its fields initialed as the Config fields
-    //ConfigBeanFactory.create[X](c, clazz)
+    //    this creates an instance of a class, initializing its fields from a Config
+    val conf2 = config.getConfigList("configuration2.field2").asScala.toList.head
+    println(conf2)            // Config(SimpleConfigObject({"list":["list2-1","list2-2"],"type":"type4"}))
+    val confList = ConfigBeanFactory.create[MyConfigList](conf2, classOf[MyConfigList])
+    println(confList.getType) // type4
+    println(confList.getList) // [list2-1, list2-2]
   }
 }
