ceb19972a584ec1fd6c51925d0c47426ecd5b3ff
kuanyu <starmap0312@gmail.com>
2018-03-08 18:25:22 +0800

    add more

--- src/test/scala/VarianceTest.scala
+++ src/test/scala/VarianceTest.scala
@@ -1,41 +1,53 @@
-// 1) we should make a mutable container invariant, otherwise run-time error may occurs
-//    ex. class Array[T](_length: Int) extends Serializable with Cloneable { ... }
-//        (i.e. Array[T] does not relate to Array[T'] for T <: T')
+// we should make a mutable container invariant, otherwise run-time error may occurs
+//   ex. class Array[T](_length: Int) extends Serializable with Cloneable { ... }
+//       (i.e. Array[T] does not relate to Array[T'] for T <: T')
 
-// 2) let Scala perform contravariant and covariant checks at compile-time
+// 1) use contravariant and covariant type checks at compile-time
 //     with the checks, we are safe to replace wherever the supertype occurs with the subtype
 //     (i.e. liskov substitution principle)
 trait IntSet
 class NonEmpty extends IntSet
 object Empty extends IntSet
 
-abstract class MyArray[-T, +R] {
-  def update(x: T): MyArray[T, R]
-  //def update(x: R)   // compile error: the variance check fails as the covariant type appears at method parameter
-  def get(): R
-  //def get(): T       // compile error: the variance check fails as the contravariant type appears at return result
-}
-// ex. MyArray[IntSet, NonEmpty] <: MyArray[IntSet, IntSet]
+// if we want an Array to be variant, i.e. Array[NonEmpty] <: Array[IntSet],
+// then we should make its methods:
+//   update() method contravariant, so that the subclass can perform the update() method where the superclass does
+//   get() method covariant       , so that the subclass can perform the update() method where the superclass does
+// ex.
+//   val arr1: Array[NonEmpty] = Array(new NonEmpty)
+//   val arr2: Array[IntSet]   = arr1 // suppose we want Array[NonEmpty] <: Array[IntSet] for NonEmpty <: IntSet
+//   arr2.update(Empty)               // this is OK if the update method is contravariant
+//   val e2: IntSet = arr2.get()      // this is OK if the get method is covariant
+// however, it can lead to contradiction if the Array is mutable
+//   val e1: NonEmpty = arr1.get()    // the element is updated to Empty, which is NOT NonEmpty
 
-class MyArraryImpl[T, R](val e: R) extends MyArray[T, R] {
-  override def update(x: T): MyArray[T, R] = {
+// the contravariant and covariant checks make sure that the up-casting is safe, and help identify the contradiction at compile time
+// i.e. MutableArray[IntSet, NonEmpty] <: MutableArray[IntSet, IntSet]
+abstract class MutableArray[-T, +R] {
+  def update(x: T): MutableArray[T, R] // we want the update method to be contravariant
+  //def update(x: R)                   //   compile error: the variance check fails as the covariant type appears at method parameter
+  def get(): R                         // we want the get method to be covariant
+  //def get(): T                       //   compile error: the variance check fails as the contravariant type appears at return result
+}
+
+class MutableArrayImpl[T, R](val e: R) extends MutableArray[T, R] {
+  override def update(x: T): MutableArray[T, R] = {
     //this.e = x       // compile error: type T does not conform to expected type R
-    this
+    this               //   the variance check helps to identify the above error at compile-time
   }
   override def get(): R = this.e
 }
-// the variance check helps to identify the above error at compile-time
 
-// 3) let Scala refer the type bounds at compile-time
-abstract class MyList[+R] {
-  def update[U >: R](x: U): MyList[U]
+// 2) use type bound reference at compile-time
+abstract class ImmutableArray[+R] {
+  def update[U >: R](x: U): ImmutableArray[U]
   def get(): R
 }
-// ex. MyList[NonEmpty] <: MyList[IntSet]
+// ex. ImmutableArray[NonEmpty] <: ImmutableArray[IntSet]
 
-class MyListImpl[T](val e: T) extends MyList[T] {
-  override def update[U >: T](x: U): MyList[U] = {
-    new MyListImpl(x)
+class ImmutableArrayImpl[T](val e: T) extends ImmutableArray[T] {
+  override def update[U >: T](x: U): ImmutableArray[U] = {
+    new ImmutableArrayImpl(x)
   }
   override def get(): T = this.e
 }
@@ -43,22 +55,25 @@ class MyListImpl[T](val e: T) extends MyList[T] {
 
 object VarianceTest {
   def main(args: Array[String]): Unit = {
-    // 1) if Array is NOT invariant, it may lead to the following contradiction
-    val a: Array[NonEmpty] = Array(new NonEmpty, new NonEmpty)
-    //val b: Array[IntSet] = a // suppose we allow Array[NonEmpty] <: Array[IntSet] for NonEmpty <: IntSet
-    //b(0) = Empty             // this is OK as if the update method is contravariant
-    val c: NonEmpty = a(0)     // this is OK as if the get method is covariant
-    // however, as Array does not check the contravariant and covariant
-    //   it leads to a run-time error if we perform these operations directly
-    //   so we should not make a mutable container variant
-
-    // 3) type bounds
-    val list1: MyList[NonEmpty] = new MyListImpl[NonEmpty](new NonEmpty)
+    // 1) use contravariant and covariant type check at compile time
+    val list1: MutableArray[IntSet, NonEmpty] = new MutableArrayImpl[IntSet, NonEmpty](new NonEmpty)
     val e1: NonEmpty = list1.get()
-    val list2: MyList[IntSet] = list1.update(Empty)
+    val list2: MutableArray[IntSet, IntSet] = list1.update(Empty)
     val e2: IntSet = list2.get()
-    //val list3: MyList[NonEmpty] = list1.update(Empty) // compile error: type MyList[IntSet] does not conform with expected type MyList[IntSet]
 
+    // the variant checks make sure the following issue at compile time
+    val list3: MutableArray[NonEmpty, NonEmpty] = new MutableArrayImpl[NonEmpty, NonEmpty](new NonEmpty)
+    val e3: NonEmpty = list3.get()
+    // val list3: MutableArray[IntSet, IntSet] = list1.update(Empty) // NOT Allowed
+    //   get compile error (type mismatch: expected NonEmpty, actual: Empty)
+
+    // 2) use type bound type check at compile time
+    val list4: ImmutableArray[NonEmpty] = new ImmutableArrayImpl[NonEmpty](new NonEmpty)
+    val e4: NonEmpty = list4.get()
+    val list5: ImmutableArray[IntSet] = list4.update(Empty)
+    val e5: IntSet = list5.get()
+    //val list5: ImmutableArray[NonEmpty] = list4.update(Empty) // NOT Allowed
+    //   get compile error (type ImmutableArray[IntSet] does not conform with expected type ImmutableArray[IntSet])
 
   }
 }
