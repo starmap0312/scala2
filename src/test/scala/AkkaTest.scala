24f6cdcce199d4a82f7dc430226bd0e7f7841b7b
kuanyu <starmap0312@gmail.com>
2018-04-03 10:25:48 +0800

    add more

--- src/test/scala/AkkaTest.scala
+++ src/test/scala/AkkaTest.scala
@@ -1,7 +1,6 @@
 import scala.concurrent.Future
 import akka.actor.{Actor, ActorLogging, Props}
 import akka.actor.Status.Failure
-import akka.event.LoggingReceive
 import akka.actor.SupervisorStrategy.Restart
 import akka.actor._
 import akka.event.LoggingReceive
@@ -37,16 +36,23 @@ object Runner {
   def props(race: Race) = Props(classOf[Runner], race)
 }
 class Runner(race: Race) extends Actor with ActorLogging {
+  // lower-level actor
+
   override def receive: Receive = LoggingReceive {
     case Start =>
       sender ! "OK"
       log.debug("running...")
-      Thread.sleep(10)
-      throw new RuntimeException("MarathonRunner is tired")
-    case Failure(throwable) => throw throwable
-    case Stop =>
+      Thread.sleep(100)
+      throw new RuntimeException("MarathonRunner is tired") // [ERROR] [akka://race/user/coach/runner] MarathonRunner is tired * 3
+                                                            // "java.lang.RuntimeException: MarathonRunner is tired"           * 3
+    case Failure(throwable) =>
+      log.debug("Runner receives Message Failure(throwable)")
+      throw throwable
+    case Stop => // never called, as we want the Actor to run forever in this example
       log.debug("stopping runner")
       context.stop(self)
+      // context.stop(self): to stop an actor actively
+      // whenever an actor is stopped, all of its children actors are recursively stopped
   }
 }
 
@@ -58,19 +64,29 @@ object Coach {
   def props(): Props = Props[Coach]
 }
 class Coach() extends Actor with ActorLogging {
+  // top-level actor
 
-  // when the Coach Actor is initialized, it executes context.actorOf which creates a new Runner and supervises it
+  // each Actor has an implicit val context: ActorContext, which can be used to create non-top-level actors
+  // when the Coach Actor is initialized, it executes context.actorOf to create a new Actor Runner and supervise it
   val runner = context.actorOf(Runner.props(new Marathon).withDispatcher("my-pinned-dispatcher"), "runner")
-  // started Actor on PinnedDispatcher: the reason is that we want the legacy process to be available
+  // the "my-pinned-dispatcher" is defined in application.conf with "type = PinnedDispatcher"
+  // Pinned dispatcher:
+  //   the dispatcher dedicates a unique thread for each actor using the thread
+  //   i.e. each actor has a dedicated thread from its own thread pool
+  //   the dispatcher is useful when the actors are doing I/O operations or performing long-running calculations
+  //   (the default is Dispatcher, where all actors share threads from the same thread pool)
 
-  // it defines supervisorStrategy for its children(Actors)
+  // we define supervisorStrategy for its children Actors
+  //   whenever an actor fails (throws an Exception) it is temporarily "suspended"
+  //   the default supervisor strategy is to "stop and restart the child", i.e. all failures result in a restart by default
   override def supervisorStrategy: SupervisorStrategy = OneForOneStrategy(maxNrOfRetries = 2, withinTimeRange = 5 seconds) {
     // it applied OneForOneStrategy which means only take action on the child that failed
-    // it tries to restart Actor 2 times with in 5 seconds and stops if Actor still fails
+    // it tries to restart the Runner Actor 2 times with in 5 seconds and stops the Runner Actor if it still fails
 
     case _: RuntimeException =>
-      // if Runner fails with RuntimeExecption, it Restart the Actor
-      sender ! Start
+      // if Runner fails with RuntimeException, it Restart the Actor
+      sender ! Start // [INFO] [akka://race/user/coach/runner] Message Start from Actor[akka://race/user/coach] to
+                     //        Actor[akka://race/user/coach/runner] was not delivered (after 2 retries)
       Restart
   }
 
@@ -87,7 +103,7 @@ object AkkaTest {
     // run the code
     val baseConfig = ConfigFactory.load()
     val system = ActorSystem.create("race", baseConfig)
-    val coach = system.actorOf(Coach.props(), "coach")
+    val coach = system.actorOf(Coach.props(), "coach") // create a top-level-actor
     coach ! StartWork
   }
 }
