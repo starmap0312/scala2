2c839f3069d9ef4b6586d19703d2d0f2e505fc92
kuanyu <starmap0312@gmail.com>
2018-04-09 13:30:54 +0800

    add more

--- src/test/scala/AkkaDaemonTest.scala
+++ src/test/scala/AkkaDaemonTest.scala
@@ -1,16 +1,8 @@
 import org.apache.commons.daemon.Daemon
 import org.apache.commons.daemon.DaemonContext
-import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
 
-class ApplicationDaemon() extends Daemon {
-  def application = new Application
-
-  def init(daemonContext: DaemonContext) {}
-  def start() = application.start()
-  def stop() = application.stop()
-  def destroy() = application.stop()
-}
-
+// an Akka actor that actually provides the service
 object ServiceActor {
   def props() = Props(new ServiceActor)
   case object Start
@@ -27,16 +19,20 @@ class ServiceActor() extends Actor with ActorLogging {
   }
 }
 
+// an Application class that constructs ActorSystem, used to create the ServiceActor
 class Application() {
-  private[this] var started: Boolean = false
   private val applicationName = "my-app"
+  private[this] var started: Boolean = false
   implicit val system = ActorSystem(s"$applicationName")
+  var service: ActorRef = ???
+  import ServiceActor.{Start, Stop}
 
   def start() {
     println(s"Starting $applicationName Service")
     if (!started) {
       started = true
-      val service = system.actorOf(ServiceActor.props(), "my-service")
+      service = system.actorOf(ServiceActor.props(), "my-service")
+      service ! Start
     }
   }
 
@@ -44,11 +40,32 @@ class Application() {
     println(s"Stopping $applicationName Service")
     if (started) {
       started = false
+      service ! Stop
       system.terminate()
     }
   }
 }
 
-object AkkaDaemonTest {
+// a Daemon class with implemented init(), start(), stop(), destroy() methods implemented
+class ApplicationDaemon() extends Daemon {
+  val application = new Application
 
+  def init(daemonContext: DaemonContext) {}
+  def start() = application.start()
+  def stop() = application.stop()
+  def destroy() = application.stop()
 }
+
+
+// one can write a init.d script to run the jsvc command as follows:
+// jsvc -Xms512m -Xmx1024m \
+//      -pidfile /var/run/myapplication.pid \
+//      -home /Library/Java/JavaVirtualMachines/jdk1.8.0_121.jdk/Contents/Home \
+//      -cp /opt/myapplication/myapplication.jar \
+//      -user yourserviceuser \
+//      -Dconfig.file=/etc/mycompany/myapplication.conf \
+//      -outfile /var/log/myapplication/myapplication_out.log \
+//      -errfile /var/log/myapplication/myapplication_err.log \
+//      -Dlogback.configurationFile=/etc/mycompany/myapplication_logback.xml \
+//      com.example.myapplication.server.ApplicationDaemon
+// the script takes arguments of start|stop
