ee996da46638d6ecadcae96a9a72f92e7e3ea24a
kuanyu <starmap0312@gmail.com>
2018-05-06 16:20:51 +0800

    add more

--- src/main/scala/functional_program_design_in_scala/Collections.scala
+++ src/main/scala/functional_program_design_in_scala/Collections.scala
@@ -1,5 +1,14 @@
 package functional_program_design_in_scala
 
 object Collections extends App {
-
+  // 2) for-expression: used to simplify flatMap(), map(), and withFilter()
+  def isEven(n: Int) = (n % 2 == 0)
+  val n = 3
+  val m = 5
+  val evenSums = for {
+    i <- (1 until n)
+    j <- (1 until m)
+    if isEven(i + j)
+  } yield (i, j)
+  println(evenSums) // Vector((1,1), (1,3), (2,2), (2,4))
 }
---
+++ src/main/scala/functional_program_design_in_scala/QueriesWithFor.scala
@@ -0,0 +1,35 @@
+package functional_program_design_in_scala
+
+case class Book(title: String, authors: List[String])
+
+object QueriesWithFor extends App {
+  // for-notation vs. database query-languages
+  // 1) representation of a mini-database: books
+  val books: List[Book] = List(
+    Book(title = "Book1: W X", authors = List("Alice", "Bob")),
+    Book(title = "Book2: X Y", authors = List("Bob",   "Cathy")),
+    Book(title = "Book3: Y Z", authors = List("Cathy", "Don"))
+  )
+  // 2) use for-notation to query book titles:
+  //    ex. find the titles of books whose authors contain Bob
+  //        SELECT title FROM books WHERE ("Bob" IN authors)
+  val titles1 = for (book <- books if book.authors contains "Bob") yield book.title
+  println(titles1) // List(Book1: W X, Book2: X Y)
+
+  //    ex. find all the book titles that contain word Y
+  //        SELECT title FROM books WHERE ("Y" IN title)
+  val titles2 = for (book <- books if (book.title indexOf "Y") >= 0) yield book.title
+  println(titles2) // List(Book2: X Y, Book3: Y Z)
+
+  //    ex. find all the book authors in the database that have written at least 2 books
+  val authors = for {
+    b1 <- books
+    b2 <- books
+    if b1.title < b2.title // this is better than filter with (b1 != b2)
+    a1 <- b1.authors
+    a2 <- b2.authors
+    if a1 == a2
+  } yield a1
+  println(authors.distinct) // List(Bob, Cathy)
+  // use .distinct() to remove duplicates, or define Books as Set[Book]
+}
