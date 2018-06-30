6957a586e6cc3ba1460cfc64148fd63014f7977d
kuanyu <starmap0312@gmail.com>
2018-06-30 20:09:04 +0800

    add more

---
+++ src/main/scala/ConfigBasics.scala
@@ -0,0 +1,19 @@
+import java.util.Map.Entry
+
+import com.typesafe.config.{ConfigFactory, ConfigValue}
+
+import scala.collection.JavaConverters._
+
+object ConfigBasics extends App {
+  val config = ConfigFactory.parseResources(getClass.getClassLoader, "application.conf").resolve
+  // 1) configObject.keySet()
+  val keys = config.getObject("envs.dev.setting").keySet().asScala.toList
+  println(keys)         // List(uri, parameters, target)
+  val parameters = config.getConfig("envs.dev.setting.parameters")
+  println(parameters)   // Config(SimpleConfigObject({"overwrite":true,"permission":774}))
+  // 2) config.entrySet() & configValue.unwrapped():
+  val params = for {
+    entry : Entry[String, ConfigValue] <- parameters.entrySet().asScala
+  } yield (entry.getKey, entry.getValue.unwrapped())
+  println(params.toMap) // Map(overwrite -> true, permission -> 774)
+}
