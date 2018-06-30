77576bfaf3b5679d30601276a01b76b1b837b1f9
kuanyu <starmap0312@gmail.com>
2018-06-30 20:46:04 +0800

    add more

--- src/main/scala/functional_program_design_in_scala/FunctionalReactiveProgramming.scala
+++ src/main/scala/functional_program_design_in_scala/FunctionalReactiveProgramming.scala
@@ -1,55 +1,5 @@
 package functional_program_design_in_scala
 
-// the Signal API (interface)
-// each Signal maintains:
-// 1) its current value
-// 2) the current expression that defines the signal value
-// 3) a set of observers: i.e. the other signals that depend on its value
-//    (if the signal changes, all observers need to be re-evaluated)
-class Signal[T](expr: => T) {
-  import Signal._
-  private var myExpr: () => T = _
-  private var myValue: T = _
-  private var observers: Set[Signal[_]] = Set()
-
-  update(expr)
-  protected def update(expr: => T): Unit = { // declare as protected as we do not want client of Signal to call update()
-    myExpr = () => expr                      // this makes Signal immutable
-    computeValue()
-  }
-  // ex. sig2() = sig1() + 5 ==> sig2.update(sig() + 5), i.e. myExpr() = () => (sig1() + 5)
-
-  protected def computeValue(): Unit = {
-    val newValue = caller.withValue(this)(myExpr()) // put this signal, ex. sig2() to the global caller stack
-    if (myValue != newValue) {
-      myValue = newValue
-      val obs = observers
-      observers = Set()
-      obs.foreach(_.computeValue())
-    }
-  }
-
-  def apply(): T = {
-    observers += caller.value                       // when sig1() + 5 gets evaluated, sig1 adds caller's head, i.e. sig2 to its observers
-    assert(!caller.value.observers.contains(this), "cyclic signal definition")
-    myValue
-  }
-  // ex. println(sig1())
-}
-object Signal {
-  // a simple implementation: i.e. use global state for caller
-  private val caller = new StackableVariable[Signal[_]](NoSignal) // initially, there is no caller
-  def apply[T](expr: => T) = new Signal(expr)
-}
-
-// the Var API (interface)
-class Var[T](expr: => T) extends Signal[T](expr) { // it adds one more method: update() to class Signal
-  override def update(expr: => T): Unit = super.update(expr) // Var should be able to call Signal's update()
-}
-object Var {
-  def apply[T](expr: => T) = new Var(expr)
-}
-
 class StackableVariable[T](init: T) {
   private var values: List[T] = List(init)
   def value: T = values.head
@@ -58,21 +8,71 @@ class StackableVariable[T](init: T) {
     try op finally values = values.tail
   }
 }
+object Signal {
+  // a simple implementation: i.e. use global state for caller
+  private val caller = new StackableVariable[Signal[_]](NoSignal) // initially, only the sentinel object, i.e. there is no caller
+  def apply[T](expr: => T) = new Signal(expr)
+}
+// the Signal API (interface), where each Signal maintains:
+// 1) its current value, i.e. var myValue: T
+// 2) the current expression that defines the signal value, i.e. var myExpr: () => T
+// 3) a set of observers: i.e. var observers: Set[Signal[_]], the other signals that depend on its value
+//    (if the signal gets updated, all observers need to be re-evaluated as well)
+class Signal[T](expr: => T) {
+  import Signal._ // import the global caller object
+  private var myValue: T = _
+  private var myExpr: () => T = _
+  private var observers: Set[Signal[_]] = Set()
 
+  update(expr)
+  protected def update(expr: => T): Unit = { // declare as protected as we do not want client of Signal to call update()
+    myExpr = () => expr                      // this makes Signal immutable
+    evaluate()
+  }
+  // ex. sig2() = Signal(sig1() + 5)
+  //   we have myExpr = () => (sig1() + 5) and then sig2's value gets evaluated()
+
+  protected def evaluate(): Unit = {
+    val newValue = caller.withValue(this)(myExpr()) // sig2 is first added to the global caller stack, then sig1() gets applied
+    if (myValue != newValue) {  // whenever the signal's value gets updated, the signal's observers' value get updated as well
+      myValue = newValue
+      val obs = observers
+      observers = Set()
+      obs.foreach(_.evaluate()) // when sig1's value gets updated, all its observers get updated as well
+    }
+  }
+
+  def apply(): T = {
+    observers += caller.value   // when sig1 gets applied, sig1 adds the caller stack head, i.e. sig2 to sig1's observers
+    assert(!caller.value.observers.contains(this), "cyclic signal definition")
+    myValue
+  }
+}
+
+// 1) the sentinel object: a special Signal that does not have value and implementation
 object NoSignal extends Signal[Nothing](???) {
-  // sentinel object: a special Signal that does not have value and implementation
-  override def computeValue(): Unit = () // override NoSignal computeValue() as it has no expression associated (???: notImplementedError)
+  override def evaluate(): Unit = () // override NoSignal evaluate() as it has no expression associated (???: notImplementedError)
+}
+// 2) the variable Signal, i.e. a Var API (interface)
+object Var {
+  def apply[T](expr: => T) = new Var(expr)
+}
+class Var[T](expr: => T) extends Signal[T](expr) {           // it adds one more method: update() to class Signal
+  override def update(expr: => T): Unit = super.update(expr) // Var should be able to call Signal's update()
 }
 
 object FunctionalReactiveProgramming extends App {
   // example1:
-  val sig = Var(3)
-  println(sig()) // 3
-  sig.update(5)  // from now on, sig returns 5 instead of 3
-  println(sig()) // 5
-  // the above can be abbreviated as the following (because of the update method)
-  // like, arr(1) = 5 is equivalent to arr.update(1, 5)
-  sig() = 5 // sig.update(5)
+  val sig1 = Var(3)
+  val sig2 = Signal(sig1() + 5)
+  println(sig1()) // 3
+  println(sig2()) // 3 + 5 = 8
+  sig1() = 5      // i.e. sig.update(5)
+  println(sig1()) // 5
+  println(sig2()) // 5 + 5 = 10
+  sig1() = 10     // i.e. sig.update(5)
+  println(sig1()) // 10
+  println(sig2()) // 10 + 5 = 10
 
   // example2: a FRP method to replace the observer pattern
   class BankAccount { // the Source which uses Signal as its state implementation
