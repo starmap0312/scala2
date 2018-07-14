12941ed75076585278d206e234c25075238b29a4
kuanyu <starmap0312@gmail.com>
2018-07-14 20:28:26 +0800

    add more

---
+++ src/main/scala/akka_actors/BankAccountExample.scala
@@ -0,0 +1,87 @@
+package akka_actors
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.event.LoggingReceive
+
+object BankAccountExample extends App {
+
+  object BankAccount {
+    case class Deposit(amount: BigInt) {  // define Message type in the companion object of the Actor class
+      require(amount > 0)
+    }
+    case class Withdraw(amount: BigInt) { // define Message type in the companion object of the Actor class
+      require(amount > 0)
+    }
+    case object Success                   // define Message object in the companion object of the Actor class
+    case object Failure                   // define Message object in the companion object of the Actor class
+  }
+  class BankAccount extends Actor {
+    import BankAccount._            // this makes the message types Failure not ambiguous with scala.util.Failure
+    var balance: BigInt = BigInt(0) // internal state
+
+    override def receive: Receive = LoggingReceive {
+      case Deposit(amount) =>
+        balance += amount
+        sender ! Success // send ack message to sender when deposit is successful
+      case Withdraw(amount) if amount <= balance =>
+        balance -= amount
+        sender ! Success
+      case _ =>
+        sender ! Failure
+    }
+  }
+
+  object WireTransfer {
+    case class Transfer(from: ActorRef, to: ActorRef, amount: BigInt)
+    case object Done
+    case object Incomplete
+  }
+  class WireTransfer extends Actor {
+    import WireTransfer._
+
+    override def receive: Receive = LoggingReceive {
+      case Transfer(from, to, amount) =>
+        from ! BankAccount.Withdraw(amount)
+        context.become(awaitWithdraw(to, amount, sender))
+    }
+
+    def awaitWithdraw(to: ActorRef, amount: BigInt, sender: ActorRef): Receive = LoggingReceive {
+      case BankAccount.Success =>
+        to ! BankAccount.Deposit(amount)
+        context.become(awaitDeposit(sender))
+      case BankAccount.Failure =>
+        sender ! Incomplete
+        context.stop(self)
+    }
+
+    def awaitDeposit(sender: ActorRef): Receive = LoggingReceive {
+      case BankAccount.Success =>
+        sender ! Done
+        context.stop(self)
+    }
+  }
+
+  class MainTask extends Actor {
+    val account1 = context.actorOf(Props[BankAccount], "account1")
+    val account2 = context.actorOf(Props[BankAccount], "account2")
+
+    account1 ! BankAccount.Deposit(100)
+
+    override def receive: Receive = LoggingReceive {
+      case BankAccount.Success =>
+        val wire = context.actorOf(Props[WireTransfer], "wire")
+        wire ! WireTransfer.Transfer(account1, account2, 50)
+        context.become(
+          LoggingReceive {
+            case WireTransfer.Done =>
+              println("Transfer done")
+              context.stop(self)
+          }
+        )
+    }
+  }
+
+  val system = ActorSystem("BankAccountExample")
+  val task = system.actorOf(Props[MainTask], "mainTask")
+  //system.terminate()
+}
