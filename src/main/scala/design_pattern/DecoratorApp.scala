73b0227c70ad463f0f9ef9d8e4dcc310851873dc
kuanyu <kuanyu@oath.com>
2021-06-10 14:03:41 +0800

    more

--- src/main/scala/design_pattern/DecoratorApp.scala
+++ src/main/scala/design_pattern/DecoratorApp.scala
@@ -8,7 +8,7 @@ package design_pattern
 //   ex. if it leads to a large number of independent subclasses for every combination
 //   ex. if a class definition is hidden and unavailable for subclassing
 
-// component
+// interface (component)
 //   the interface for objects that have specific responsibilities
 trait Coffee {
 
@@ -17,7 +17,7 @@ trait Coffee {
   def getDescription: String
 }
 
-// concrete component
+// base class (concrete component)
 //   an class that implements every responsibilities of the component interface
 class SimpleCoffee extends Coffee {
 
@@ -26,16 +26,20 @@ class SimpleCoffee extends Coffee {
   override def getDescription: String = "simple coffee"
 }
 
-// concrete decorator
+// decorator (stackable trait)
 //   it maintains a reference to a component object and it conforms to component's interface as well
-//   `abstract override` is needed as the base class method is abstract (a trait)
+//   it extends the interface which means that the trait can only be mixed into a class that also extends the interface
 trait MilkAdded extends Coffee {
   abstract override def getCost: Int = super.getCost + 5
+  // the modifier `abstract` is needed as it's super is a trait (interface)
+  //   the modifier tells the compiler the trait will be mixed into some base class with concrete implementation of the method
+  //   without the `abstract` modifier, you'll receives a compile error
+  //  the super call will be dynamically bound: it will work as long as the trait is mixed in with a base class (concrete component)
 
   abstract override def getDescription: String = s"${super.getDescription} with milk"
 }
 
-// concrete decorator
+// decorator
 trait SugarAdded extends Coffee {
   abstract override def getCost: Int = super.getCost + 3
 
@@ -43,16 +47,14 @@ trait SugarAdded extends Coffee {
 }
 
 object DecoratorApp extends App {
-  val coffee1 = new SimpleCoffee with SugarAdded
+  val coffee1 = new SimpleCoffee with SugarAdded // i.e. SugarAdded(SimpleCoffee)
   println(coffee1.getCost) // 18 = 10 + 3
   println(coffee1.getDescription) // simple coffee with sugar
 
   // scala trait mixin
-  //   ex. new trait A with B with C
-  //       A -> B -> C is constructed in sequence
-  //         i.e. a wrapped object new C(B(A)) is created
-  //       C.method() -> B.method() -> A.method() is called in sequence when the wrapped object's method() is called
-  //         i.e. when the wrapped object C(B(A)).method() is called
+  //   ex. trait A with B with C
+  //       i.e. C(B(A)), a wrapped object, is created: A -> B -> C is constructed in sequence
+  //       when C(B(A)).method() is called, C.method() -> B.method() -> A.method() is called in sequence
   val coffee2 = new SimpleCoffee with MilkAdded with SugarAdded
   println(coffee2.getCost) // 18 = 10 + 5 + 3, i.e. SugarAdded(MilkAdded(SimpleCoffee)).getCost()
   println(coffee2.getDescription) // simple coffee with milk with sugar, i.e. SugarAdded(MilkAdded(SimpleCoffee)).getDescription()
---
+++ src/main/scala/design_pattern/StackableTraitApp.scala
@@ -0,0 +1,71 @@
+package design_pattern
+
+import scala.collection.mutable.ArrayBuffer
+
+// https://www.artima.com/articles/scalas-stackable-trait-pattern
+// this pattern is similar in structure to the decorator pattern
+//   it differs in that it involves decoration of class composition instead of object composition
+//   i.e. a stackable trait decorates the core trait at compile time
+//        a decorator object decorates the core object at run time
+// in this pattern, a trait (or class) can play one of three roles:
+//
+//      base
+//    --|  |--
+//    |       |
+//  core  stackable
+//
+// 1) base trait (an interface that defines abstract methods)
+// 2) core trait (a concrete class that implements all the abstract methods defined in the interface)
+// 3) stackable trait (a decorator class that overrides the abstract methods defined in the interface)
+//      it uses scala's `abstract override` modifiers and invokes the `super` implementation of the same method
+//      as a result, the stackable modifies the core's behavior
+// you can select any of the stackables & mix them into a class to obtain a new class that has all of the modifications you chose
+
+// interface (base trait)
+trait IntQueue {
+  def get(): Int
+  def put(x: Int)
+}
+
+// base class (core trait)
+class BasicIntQueue extends IntQueue {
+
+  private val buffer = new ArrayBuffer[Int]
+
+  def get() = buffer.remove(0)
+
+  def put(x: Int) = {
+    buffer += x
+  }
+}
+
+// decorator (stackable trait)
+//   it modifies the behavior of an underlying (super) core trait rather than defining a concrete class itself
+trait Doubling extends IntQueue {
+
+  abstract override def put(x: Int) = {
+    super.put(2 * x)
+  }
+}
+
+// decorator (stackable trait)
+trait Incrementing extends IntQueue {
+
+  abstract override def put(x: Int) = {
+    super.put(x + 1)
+  }
+}
+
+object StackableTraitApp extends App {
+
+  val queue = new BasicIntQueue with Doubling with Incrementing
+  // i.e. queue = new Incrementing(new Doubling(new BasicIntQueue()))
+  //      when you make a call to queue.put(0)
+  //      it makes a call to Incrementing's put() method, in which its decoratee's put() method is called via super.put()
+  queue.put(0) // put (2 *(0 + 1)) = 2
+  queue.put(1) // put (2 *(1 + 1)) = 4
+  queue.put(2) // put (2 *(2 + 1)) = 6
+  println(queue.get()) // 2
+  println(queue.get()) // 4
+  println(queue.get()) // 6
+}
