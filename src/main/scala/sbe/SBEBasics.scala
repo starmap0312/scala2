bdbb57cbaadd7f4f7d7b63238f4dee8d80ed4226
kuanyu <kuanyu@oath.com>
2021-11-03 16:29:11 +0800

    more

--- src/main/scala/sbe/SBEBasics.scala
+++ src/main/scala/sbe/SBEBasics.scala
@@ -1,5 +1,11 @@
 package sbe
-// https://github.com/real-logic/simple-binary-encoding
+
+import com.my.sbe._
+import org.agrona.concurrent.UnsafeBuffer
+import java.nio.ByteBuffer
+
+// https://aeroncookbook.com/simple-binary-encoding/basic-sample/
+// ref: https://github.com/real-logic/simple-binary-encoding
 // wiki: https://github.com/real-logic/simple-binary-encoding/wiki
 // Java Users Guide: https://github.com/real-logic/simple-binary-encoding/wiki/Java-Users-Guide
 // SBE:
@@ -28,5 +34,58 @@ package sbe
 //       SBE schemas support the concept of an offset that defines the starting position of a field within a message
 //       it is assumed the messages are encapsulated within a framing protocol on 8 byte boundaries
 object SBEBasics extends App {
-  println("hello")
+  // 1) Encoding
+  // construct the two encoders required
+  val encoder: SampleMessageEncoder = new SampleMessageEncoder()
+  println(s"encoder.encodedLength=${encoder.encodedLength}") // encoder.encodedLength=0
+
+  // 1.1) build the message header and actual header objects: this can be done once and reused if desired
+  val headerEncoder: MessageHeaderEncoder = new MessageHeaderEncoder()
+
+  // 1.2) allocate memory for the encoded data to be written to
+  //      allocate memory and an unsafe buffer to act as a destination for the data
+  val byteBuffer: ByteBuffer = ByteBuffer.allocateDirect(128)
+  val directBuffer: UnsafeBuffer = new UnsafeBuffer(byteBuffer)
+
+  // 1.3) wrap the buffer, and apply the header
+  //      so that later, we can use the encoder to write the encoded output to the direct buffer
+  encoder.wrapAndApplyHeader(directBuffer, 0, headerEncoder)
+  println(s"encoder.encodedLength=${encoder.encodedLength}") // encoder.encodedLength=12
+
+  // 1.4) set the data fields as desired
+  //      use the encoder to set the fields to desired values
+  encoder.sequence(123)
+  encoder.enumField(SampleEnum.VALUE_1)
+  encoder.message("a message")
+  println(s"encoder.encodedLength=${encoder.encodedLength}") // encoder.encodedLength=25
+
+  val encodedLength = MessageHeaderEncoder.ENCODED_LENGTH + encoder.encodedLength // encodedLength=33
+  println(s"encodedLength=$encodedLength") // 33
+
+  // 2) Decoding
+  // 2.1) construct the decoder and message header decoder
+  val decoder = new SampleMessageDecoder
+  val headerDecoder = new MessageHeaderDecoder
+
+  // 2.2) the header decoder wraps the inbound buffer, and reads the header
+  var bufferOffset: Int = 0
+  headerDecoder.wrap(directBuffer, bufferOffset)
+
+  // 2.3) safety check can be done at this point to verify that the correct decoder is being used
+  //      lookup the applicable flyweight to decode this type of message based on templateId and version
+  val templateId = headerDecoder.templateId
+  if (templateId != SampleMessageDecoder.TEMPLATE_ID) throw new IllegalStateException("Template ids do not match")
+  val actingBlockLength: Int = headerDecoder.blockLength
+  val actingVersion: Int = headerDecoder.version
+  println(templateId, actingBlockLength, actingVersion) // (1,12,1)
+
+  // 2.4) next, wrap the inbound buffer
+
+  bufferOffset += headerDecoder.encodedLength
+  println(s"bufferOffset: $bufferOffset") // bufferOffset: 8
+  decoder.wrap(directBuffer, bufferOffset, actingBlockLength, actingVersion)
+
+  println(s"decoder.sequence=${decoder.sequence}") // decoder.sequence=123
+  println(s"decoder.enumField=${decoder.enumField}") // decoder.enumField=VALUE_1
+  println(s"decoder.message=${decoder.message}") // decoder.message=a message
 }
