bdb42cc3968fade74a97ddc2750feeacfb87b97e
kuanyu <kuanyu@oath.com>
2022-02-09 14:56:11 +0800

    more

--- src/main/scala/MyCollections.scala
+++ src/main/scala/MyCollections.scala
@@ -52,4 +52,34 @@ trait MyIterable[+A] extends  MyIterableOps[A, MyIterable, MyIterable[A]]// with
 object MyCollections extends App {
   println("hello")
 
+  // another example
+  trait BoxOps[+A, +CC[_]] {
+    def map[B](f: A => B): CC[B] // ex. Box[B]
+    def iterableFactory: BoxFactory[CC] // used to produce the same Collection type for map()
+  }
+
+  trait BoxFactory[+CC[_]] {
+    def from[B](item: B): CC[B] // a factory that produces the same Collection type
+  }
+
+  class Box[A](item: A) extends BoxOps[A, Box] {
+
+    override def map[B](f: A => B): Box[B] = iterableFactory.from(f(item)) // use the factory to produce the same Collection type with a different item
+
+    override def iterableFactory: BoxFactory[Box] = new BoxFactory[Box] {
+      override def from[B](e: B): Box[B] = new Box(e)
+    }
+
+    // or use another defined method: fromSpecific(f(item)) for map()
+//    def fromSpecific[B](item: B): Box[B] = iterableFactory.from(item)
+
+    override def toString = s"Box($item)"
+  }
+
+  val box = new Box(1)
+  val intBox: Box[Int] = box.map(_ + 1)
+  val strBox: Box[String] = box.map(_.toString + " string")
+  println(intBox) // Box(1)
+  println(strBox) // Box(1 string)
+
 }
