4df3cc1936844b6bf391057c510905112ed2bf8a
kuanyu <kuanyu@yahooinc.com>
2022-09-12 11:42:30 +0800

    more

--- src/main/java/JavaReactor.java
+++ src/main/java/JavaReactor.java
@@ -2,11 +2,19 @@ import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 import java.util.Optional;
-import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 
 public class JavaReactor {
+
+    private static String apply(String x) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return x;
+    }
 
     String methodThatThrowsExecption(String input) {
         if (input.isEmpty()) throw new RuntimeException("input is empty");
@@ -24,6 +32,7 @@ public class JavaReactor {
         // 1.1) subscribe
         //    subscribe Consumer to the Mono
         //    i.e. consume all the elements in the sequence, handle errors and react to completion.
+        System.out.println("1.1)");
         mono.subscribe(
             (it)-> System.out.println("OnNext"),   // OnNext
             (ex) -> System.out.println("OnError"),  // OnError
@@ -35,6 +44,7 @@ public class JavaReactor {
         System.out.println();
 
         // 1.2) subscribe vs. block
+        System.out.println("1.2)");
         System.out.println("start Thread: " + Thread.currentThread().getName());
         Mono<Integer> optionalMono1 = Mono.just(1);
         optionalMono1
@@ -52,6 +62,27 @@ public class JavaReactor {
         // subscribe Thread: main
         // end Thread: main
 
+        // 1.3)
+        System.out.println("1.3)");
+        System.out.println("start of main: tread name: " + Thread.currentThread().getName());
+        Mono.just("123")
+            .map(JavaReactor::apply)
+            .doOnNext(input -> System.out.println("  in mono subscribe: tread name: " + Thread.currentThread().getName()))
+            .subscribeOn(Schedulers.boundedElastic())
+            .doOnNext(input -> System.out.println("  in mono subscribe: tread name: " + Thread.currentThread().getName()))
+            .subscribe();
+        System.out.println("end of main: tread name: " + Thread.currentThread().getName());
+        System.out.println("wait for async result: " + Thread.currentThread().getName());
+        Thread.sleep(3000);
+        System.out.println();
+        // start of main: tread name: main
+        // end of main: tread name: main
+        // wait for async result: main
+        //   in mono subscribe: tread name: boundedElastic-1
+        //   in mono subscribe: tread name: boundedElastic-1
+
+        // 1.4)
+        System.out.println("1.4)");
         System.out.println("start Thread: " + Thread.currentThread().getName());
         Mono<Integer> optionalMono2 = Mono.just(1);
         optionalMono2
@@ -71,13 +102,14 @@ public class JavaReactor {
         // end Thread: main
 
         // 2) basics
+        System.out.println("2)");
         var empty = Mono.empty();
         System.out.println(empty.block());                                            // null
         System.out.println(empty.blockOptional());                                    // Optional.empty
         System.out.println(Mono.justOrEmpty(Optional.empty()).blockOptional()); // Optional.empty
         System.out.println();
 
-        // 3) mono w/ blocking calls
+        // 3.1) mono w/ blocking calls
         Function<String, String> blockingCall = (String input) -> {
             try {
                 System.out.println("    in blockingCall: tread name: " + Thread.currentThread().getName());
@@ -88,6 +120,7 @@ public class JavaReactor {
             return input;
         };
 
+        System.out.println("3.1)");
         System.out.println("start of main: tread name: " + Thread.currentThread().getName());
         Mono.just("123")
             .doOnNext(input -> System.out.println("  in mono: tread name: " + Thread.currentThread().getName()))
@@ -103,6 +136,8 @@ public class JavaReactor {
         // end of main: tread name: main
         System.out.println();
 
+        // 3.2)
+        System.out.println("3.2)");
         System.out.println("start of main: tread name: " + Thread.currentThread().getName());
         Mono.just("123")
             .doOnNext(input -> System.out.println("c" + Thread.currentThread().getName()))
@@ -118,6 +153,8 @@ public class JavaReactor {
         //   in mono: tread name: boundedElastic-1    <- continue to work on the separate thread
         // end of main: tread name: main
 
+        // 3.3)
+        System.out.println("3.3)");
         System.out.println("start of main: tread name: " + Thread.currentThread().getName());
         Mono.just("123")
             .doOnNext(input -> System.out.println("  in mono: tread name: " + Thread.currentThread().getName()))
@@ -133,7 +170,8 @@ public class JavaReactor {
         //   in mono: tread name: boundedElastic-1    <- continue to work on the separate thread
         // end of main: tread name: main
 
-
+        // 3.4)
+        System.out.println("3.4)");
         System.out.println("start of main: tread name: " + Thread.currentThread().getName());
         Mono.just("123")
             .doOnNext(input -> System.out.println("  in mono: tread name: " + Thread.currentThread().getName()))
